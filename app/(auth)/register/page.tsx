0c405483f7ee0f3f9da605b99d8564204e24e24e
DEV <tapesdd@google.com>
2026-09-23 04:46:44 +0200

    fix: add force-dynamic to register page wrapper

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,8 +1,12 @@
 import { headers } from "next/headers";
 import RegisterForm from "./RegisterForm";
 
+export const dynamic = "force-dynamic";
+
 headers(); // Force dynamic rendering (SSR)
 
 export default function RegisterPage() {
+  return <RegisterForm />;
+}
   return <RegisterForm />;
 }
